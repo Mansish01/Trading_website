bac3afe86f5e96bb57ba46f1b52835b527f46278
Sonugc <Sonugc71@gmail.com>
2024-05-02 21:16:15 +0545

    commoditied updated

--- src/Commodities.js
+++ src/Commodities.js
@@ -2,7 +2,7 @@ export const Products = [
     {
         id : 1,
         productname : "EDIBLE OIL",
-        description : "orem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        description : "GC Trade specializes in trading edible oils such as palm, soybean, sunflower, canola, and corn oil. ",
         productimage :"/product-images/edibleoil.png",
 
     },
@@ -10,7 +10,7 @@ export const Products = [
     {
         "id":2,
         "productname":"FEEDSTUFF",
-        "description" : "orem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. orem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "description" : "GC Trade specializes in trading feedstuff commodities essential for animal nutrition, including soybean meal, corn gluten meal, sunflower meal, canola meal, and palm kernel meal. ",
         "productimage": "/product-images/feedstuff.png",
         "productimage": "/product-images/feedstuff.png",
     },
@@ -18,28 +18,28 @@ export const Products = [
     {
         "id":3,
         "productname":"FERTILIZERS",
-        "description" : "orem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "description" : " GC Trade includes a range of fertilizers such as nitrogen-based, phosphorus-based, and potassium-based fertilizers, as well as specialty blends tailored to specific crop needs. ",
         "productimage": "/product-images/fertilizers.png",
     },
 
     {
         "id":4,
         "productname":"MILK PRODUCTS",
-        "description" :"orem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "description" :"GC Trade specializes in trading milk products, offering a diverse range of dairy commodities  such as milk powder, cheese, butter, yogurt, and whey protein.",
         "productimage": "/product-images/milkproducts.png",
     },
 
     {
         "id":5,
         "productname":"OILSEEDS",
-        "description" : "orem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "description" : "GC Trade specializes in trading oilseed commodities  such as soybeans, sunflower seeds, canola seeds, and palm kernels. ",
         "productimage": "/product-images/oilseeds.png",
     },
 
     {
         "id":6,
-        "productname":"WHEAT",
-        "description" : "orem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "productname":"CEREALS",
+        "description" : "GC Trade specializes in trading cereal commodities such as wheat, corn, rice, barley, and oats. ",
         "productimage": "/product-images/wheat.png",
     },
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <div className='headertext'>
           <p className='headertext3'>"WELCOME TO GC TRADE"</p>
           <p className='headertext1'>YOUR GATEWAY TO</p>
-          <p className='headertext2'>global commodity</p>
+          <p className='headertext2'>Global Commodity</p>
         </div>
     </div>
    
